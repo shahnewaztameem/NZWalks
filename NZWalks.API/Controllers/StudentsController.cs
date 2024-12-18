b1f7ad545e72b941151760ce7540f61459d0718c
shahnewaz tameem <shahnewaztamim@gmail.com>
2024-12-18 23:50:09 +0600

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:PORT/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Shahnewaz", "Gazi", "Fowzia", "Rodela" };
+
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        // db options pass to contructor
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        // create DB set (property of db context class) - collection of entities
+        // these 3 props will create tables inside new db
+        public DbSet<Difficulty> Difficulties{ get; set; }
+
+        public DbSet<Region> Regions{ get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
